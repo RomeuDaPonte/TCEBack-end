8a8b8b125932a0598da2c97636f29f1732090f33
RomeuDaPonte <57477053+RomeuDaPonte@users.noreply.github.com>
2020-06-10 16:25:56 +0100

    implemented getTotalSpentByDate

--- routes/expense.js
+++ routes/expense.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { Expense, validateNewExpense } = require("../models/expense");
+const {
+  Expense,
+  validateNewExpense,
+  getTotalAmonth,
+} = require("../models/expense");
+
+const { User } = require("../models/user");
 
 router.post("/", async (req, res) => {
   const { error } = validateNewExpense(req.body);
@@ -9,6 +15,26 @@ router.post("/", async (req, res) => {
   await expense.save();
 
   res.send(expense);
+});
+
+router.get("/getTotalSpentByDate", async (req, res) => {
+  const exists = await User.findOne({ _id: req.body.userId });
+  if (!exists) return res.status(400).send("Invalid user id");
+
+  var date = new Date();
+  var firstDayInCurrentMonth = new Date(date.getFullYear(), date.getMonth(), 1);
+
+  const arrayOfAmounts = await Expense.find()
+    .and({ userId: req.body.userId })
+    .and({ date: { $gt: firstDayInCurrentMonth } })
+    .select({ amount: 1 })
+    .select({ _id: 0 });
+
+  const totalAmount = {
+    totalSpent: getTotalAmonth(arrayOfAmounts),
+  };
+
+  res.send(totalAmount);
 });
 
 module.exports = router;
